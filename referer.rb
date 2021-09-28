2da7536e5a3eb990aafbe4b43c310a5892ac6814
Alberto Tiboni <a.tiboni@gmail.com>
2021-09-28 16:49:01 +0200

    cosmetics

--- referer.rb
+++ referer.rb
@@ -227,9 +227,9 @@ def try_matching(node, child, headers, found, missing, doc)
             end
         end
     
-        if !matched 
+        if !matched && !(node[:class] && node[:class].include?('heading'))
             # && !search.include?('Pozzi.petizioni') 
-            && !(node[:class] && node[:class].include?('heading'))
+            
             # puts 'NOT FOUND'
             ap search
             # ap node[:id]
