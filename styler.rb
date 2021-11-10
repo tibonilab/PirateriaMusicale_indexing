a6c440da691c07fe7f170088874919778295cfb1
Alberto Tiboni <a.tiboni@gmail.com>
2021-11-10 18:54:40 +0100

    better styler

--- styler.rb
+++ styler.rb
@@ -14,86 +14,90 @@ doc.search('p').each do |node|
     # ap node
     # ap node.child
 
-    if node.child[:style] == 'font-size:14pt;font-weight:bold'
+    if node.child
 
-        if !h1.any? { |s| node.child.text.strip.include?(s) }
-            h1 << node.child.text.strip
+        if node.child[:style] == 'font-size:14pt;font-weight:bold'
 
-            node[:class] = 'heading1'
-        else 
-            node[:class] = 'heading2'
-        
-        end
-        # ap node
-    end
+            if !h1.any? { |s| node.child.text.strip.include?(s) }
+                h1 << node.child.text.strip
 
-
-    if node.child[:style] == 'font-size:14pt;font-weight:bold;color:E7E6E6'
-        node[:class] = 'heading2'
-
-        # ap node
-    end
-
-    if node.child[:style] == 'font-weight:bold;color:44546A' && node[:style] == 'text-align:center;'
-        node[:class] = 'heading3'
-
-        # ap node
-    end
-
-    if node.child[:style] == 'font-weight:bold;color:44546A' && h1.any? { |s| s.include? (node.parent.child.text.strip) }
-        node[:class] = 'heading3'
-
-        # ap node
-    end
-
-    if node.child[:style] == 'font-size:11pt;font-style:italic'
-        node.child[:class] = 'heading4'
-
-        # ap node.child
-    end
-
-
-    if node.child[:style] == 'font-size:11pt;text-decoration:underline'
-        node[:class] = 'heading5'
-
-        # ap node.child
-    end
-
-
-    if node.child[:style] == 'font-size:11pt;font-weight:bold'
-        node[:class] = 'heading6'
-
-        # ap node.child
-    end
-
-
-    if node.child[:style] == 'font-size:10pt;font-weight:bold;color:44546A'
-        if node.children[1]
-            if node.children[1].inner_html != 'Federico Ricci'
-            node[:class] = 'heading6'
-            end
-        else
-            node[:class] = 'heading6'
-        end
-
-        # ap node.child
-    end
-
-    if node.child[:style] == 'text-decoration:underline;color:44546A'
-        node[:class] = 'heading5'
-
-        # ap node.child
-    end
-
-    node.children.each do |child|
-        if node.child.text.start_with?('*')
+                node[:class] = 'heading1'
+            else 
+                node[:class] = 'heading2'
             
-            if child[:style] == 'font-size:11pt;font-weight:bold'
-                node[:class] = 'heading6'
+            end
+            # ap node
+        end
 
-                # ap child.text
+
+        if node.child[:style] == 'font-size:14pt;font-weight:bold;color:E7E6E6'
+            node[:class] = 'heading2'
+
+            # ap node
+        end
+
+        if node.child[:style] == 'font-weight:bold;color:44546A' && node[:style] == 'text-align:center;'
+            node[:class] = 'heading3'
+
+            # ap node
+        end
+
+        if node.child[:style] == 'font-weight:bold;color:44546A' && h1.any? { |s| s.include? (node.parent.child.text.strip) }
+            node[:class] = 'heading3'
+
+            # ap node
+        end
+
+        if node.child[:style] == 'font-size:11pt;font-style:italic'
+            node.child[:class] = 'heading4'
+
+            # ap node.child
+        end
+
+
+        if node.child[:style] == 'font-size:11pt;text-decoration:underline'
+            node[:class] = 'heading5'
+
+            # ap node.child
+        end
+
+
+        if node.child[:style] == 'font-size:11pt;font-weight:bold'
+            node[:class] = 'heading6'
+
+            # ap node.child
+        end
+
+
+        if node.child[:style] == 'font-size:10pt;font-weight:bold;color:44546A'
+            if node.children[1]
+                if node.children[1].inner_html != 'Federico Ricci'
+                node[:class] = 'heading6'
+                end
+            else
+                node[:class] = 'heading6'
+            end
+
+            # ap node.child
+        end
+
+        if node.child[:style] == 'text-decoration:underline;color:44546A'
+            node[:class] = 'heading5'
+
+            # ap node.child
+        end
+
+        node.children.each do |child|
+            if node.child.text.start_with?('*')
+                
+                if child[:style] == 'font-size:11pt;font-weight:bold'
+                    node[:class] = 'heading6'
+
+                    # ap child.text
+                end
             end
         end
+
     end
 end
 
