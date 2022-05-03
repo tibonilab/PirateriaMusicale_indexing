4de9e7027103278fd7a4e45f3ede233de445c058
Alberto Tiboni <a.tiboni@gmail.com>
2022-05-03 18:37:51 +0200

    better bash script

--- indexer.sh
+++ indexer.sh
@@ -18,3 +18,12 @@ ruby styler.rb
 echo "Performing referer";
 ruby referer.rb
 
+echo "Performing final touches"
+sed -i -e 's@color:@color:#@gi' ./output/output-tagged-full-text-styled-refered.html
+sed -i -e 's@ @ @gi' ./json/fulltext.json
+sed -i -e 's@ @ @gi' ./json/toc.json
+sed -i -e 's@ @ @gi' ./json/index.json
+
+echo "Copy generated files"
+cp ./output/output-tagged-full-text-styled-refered.html ../PirateriaMusicale/dev/dataset/output.html
+cp ./json/*.json ../PirateriaMusicale/dev/dataset/
